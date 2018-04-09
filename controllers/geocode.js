826237dd2f362b32e61a0a59d8e5fef8b91824b7
Apuroop Naidu <apuroop.naidu@gmail.com>
2018-04-09 14:42:55 -0500

    The first commit in Node JS Weather Application.

---
+++ controllers/geocode.js
@@ -0,0 +1,45 @@
+const request = require('request');
+
+module.exports.geocodeAddress = (inputAddress, callback) => {
+    var encodedAddress = encodeURIComponent(inputAddress);
+    request({
+        url:`https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyAXoS1eW90dFaCKSbzIgW_XYshsS9gWSeE`,
+        json: true
+    }, (err, res, body) => {
+        if(err){
+            callback("Unable to connect to Google Servers. Try Again Later.");
+        } else if(body.status === "ZERO_RESULTS"){
+            callback("That was an invalid entry. Try Again.");
+        } else if(body.results[0].address_components[0].types[0] === "country"){
+            callback("Can't fetch results for the type 'Country'. Try Again.");
+        } else if(body.status === "OK"){
+            callback(undefined, {
+                Address: body.results[0].formatted_address,
+                Latitude: body.results[0].geometry.location.lat,
+                Longitude: body.results[0].geometry.location.lng,
+                City: body.results[0].address_components[1].short_name,
+                State: body.results[0].address_components[3].long_name
+            });
+        }
+    });
+};
+
+module.exports.getAllDetails = (lat, long, callback) => {
+    request({
+        url: `https://api.darksky.net/forecast/fcccd3131bf7fb558373edef0c1e65ce/${lat},${long}`,
+        json: true
+    }, (err, res, body) => {
+        if(err){
+            callback("Unable to connect to Google Servers.");
+        } else {
+            callback(undefined, {
+                temperature: body.currently.temperature,
+                apparentTemperature: body.currently.apparentTemperature,
+                summary: body.currently.summary,
+                humidity: body.currently.humidity,
+                precipIntensity: body.currently.precipIntensity,
+                windSpeed: body.currently.windSpeed
+            });
+        }
+    });
+};
---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const geocode = require('./controllers/geocode');
+
+app.use(cors());
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/tether/dist/js'));
+app.use('/dist', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/', (req, res) => {
+    location = JSON.stringify(req.body);
+    geocode.geocodeAddress(location, (error, result) => {
+        if(error){
+            res.json(error);
+        } else {
+            geocode.getAllDetails(result.Latitude, result.Longitude, (err, result1) => {
+                if(err){
+                    res.json(err);
+                } else {
+                    res.render("details", {
+                        item : result, //used in details.ejs
+                        item1 : result1 //used in details.ejs
+                    });
+                }
+            });
+        }
+    });
+});
+
+app.listen('5001', () => {
+    console.log("Application running on http://localhost:5001/");
+});
+
+module.exports = app;
